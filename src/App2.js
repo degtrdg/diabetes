16d89c963cdb1ca624ca7f1784089bb80649e4dc
deanhunt7 <dwhuntmail7@github.io>
2021-11-06 10:51:13 -0500

    Made button functional component, currently passes in answer1, when pressed it logs current value of answer1

--- src/App2.js
+++ src/App2.js
@@ -12,7 +12,10 @@ function Question(props) {
   // pass state to the parent
   // each question will have an answer
   // you're alowed to submit the form
-  const [answer, setAnswer] = useState("unanswered");
+
+  var answer = props.answer;
+  var setAnswer = props.setQuestion;
+//  [answer, setAnswer] = useState("unanswered");
 
   if (props.answerType === "integer") {
     return (
@@ -27,7 +30,10 @@ function Question(props) {
                   <input
                     type="number"
                     value={answer}
-                    onChange={(e) => setAnswer(e.target.value)}
+                    onChange={(e) => {
+                      setAnswer(e.target.value);
+                    }
+                    }
                   />
                 </div>
               </div>
@@ -38,8 +44,8 @@ function Question(props) {
     );
   } else {
     return (
-      <div style={{ display: 'flex', justifyContent: 'center' }}> {/*added inline css styling, flex is basically any element, but it is much more customizable in terms
-      of placement, so it was nice in this case to just be able to place it in the middle. JustifyContent is self-explanatory*/}
+      <div style={{ display: 'flex', justifyContent: 'center' }}> 
+      {/*added inline css styling, flex is basically any element, but it is much more customizable in terms of placement, so it was nice in this case to just be able to place it in the middle. JustifyContent is self-explanatory*/}
         <div className="col-6 pt-3 pb-3"> {/*Basically there are 12 columns on a webpage for react, so I just said that I want my card to take up 6 of these 12. That means there
         are 3 empty columns on each side since we justified it to the middle.*/}
           <div className="row"> {/* added a row next */}
@@ -48,7 +54,7 @@ function Question(props) {
                 <div className="card-text pt-3">
                   <h3>{props.questionText}</h3>
                   <br />
-                  <div className="row pb-3"> 
+                  <div className="row pb-3">
                     <div className="col-6">
                       <div>
                         <input
@@ -82,13 +88,17 @@ function Question(props) {
   }
 }
 
-function SubmitButton(){
+function SubmitButton(props) {
   var getSubmit = () => {
-    console.log("pls submit");
+    if(props.answer1 !== "unanswered"){
+      console.log(props.answer1);
   }
-  return(
-        <button onClick={getSubmit}>Submit</button>
-        // <button onClick={checked ? SubmitEvent() : SubmitEvent()}>Submit</button>
+  else console.log("unanswered");
+}
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', paddingTop: "0.6em", paddingBottom: "1.5em" }}>
+      <button onClick={getSubmit}>Submit</button>
+    </div>
   )
 }
 export default function App() {
@@ -126,31 +136,33 @@ export default function App() {
     }
   }
 
-    // this returns an array of question components
-    return (
-      <div className="app">
-        <Question
-          answer={answer1}
-          questionText={questions[0].questionText}
-          answerType={questions[0].answerType}
-        />
-        <Question
-          answer={answer2}
-          questionText={questions[1].questionText}
-          answerType={questions[1].answerType}
-        />
-        <Question
-          answer={answer3}
-          questionText={questions[2].questionText}
-          answerType={questions[2].answerType}
-        />
-        <Question
-          answer={answer4}
-          questionText={questions[3].questionText}
-          answerType={questions[3].answerType}
-        />
-        <SubmitButton
-        />
-      </div>
-    );
+  // this returns an array of question components
+  return (
+    <div className="app">
+      <Question
+        answer={answer1}
+        setQuestion={setQuestionsAnswered1}
+        questionText={questions[0].questionText}
+        answerType={questions[0].answerType}
+      />
+      <Question
+        answer={answer2}
+        questionText={questions[1].questionText}
+        answerType={questions[1].answerType}
+      />
+      <Question
+        answer={answer3}
+        questionText={questions[2].questionText}
+        answerType={questions[2].answerType}
+      />
+      <Question
+        answer={answer4}
+        questionText={questions[3].questionText}
+        answerType={questions[3].answerType}
+      />
+      <SubmitButton
+      answer1={answer1}
+      />
+    </div>
+  );
 }
