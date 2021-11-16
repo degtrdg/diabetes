a91cd661ca4e8a6155425ed15001d09b2d310b02
deanhunt7 <dwhuntmail7@github.io>
2021-11-15 20:44:44 -0600

    removed contact

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { useState, Button } from "react";
 import Quiz from "./Quiz";
 import About from "./About";
-import Contact from "./Contact";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import navbg from "./images/navbg.jpg";
 import Welcome from "./Welcome";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -22,16 +20,12 @@ export default function App() {
             <li className="px-3">
               <Link to="/about">About</Link>
             </li>
-            <li className="px-3">
-              <Link to="/contact">Contact</Link>
-            </li>
           </ul>
         </nav>
       </div>
       <hr />
       <div>
         <Routes>
-          <Route path="/contact" element={<Contact />}></Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="/quiz" element={<Quiz />}></Route>
           <Route path="/" element={<Welcome />}></Route>
